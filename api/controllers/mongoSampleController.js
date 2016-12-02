5bdddd4574e9fe2f47ad54142bf4e8c6a876bc70
Shane <shane@Shanes-MacBook-Pro.local>
2016-12-02 13:55:49 -0800

    add mongo db try out

--- api/controllers/mongoSampleController.js
+++ api/controllers/mongoSampleController.js
@@ -1,7 +1,31 @@
+var db = require('monk')('localhost/shanePrototypeMongo');
+
 module.exports = {
 	retrieve: function (req, res) {
+		var samples = db.get('samples');
+
+		samples.find({},{},function(err, samples){
+			if(err){
+				res.send(err);
+			} else {
+				res.send(JSON.stringify(samples));
+			}
+		});
  	},
 
  	create: function (req, res) {
+		var posts = db.get('samples');
+
+		posts.insert({
+			"name": req.body.name,
+			"age": req.body.age,
+			"gender": req.body.gender
+		}, function(err, post){
+			if(err){
+				res.send(err);
+			} else {
+				res.send(req.body);
+			}
+		});
  	}
 }
