e603128452fc7e388f71006e2fb434fce0b8484e
Shane <shane@Shanes-MacBook-Pro.local>
2016-12-03 15:48:42 -0800

    add mongo sample test

--- tests/02-mongoSampleTest.js
+++ tests/02-mongoSampleTest.js
@@ -1,13 +1,33 @@
 var should = require("should");
-var request = require('request');
+var request = require('supertest');
 var expect = require("chai").expect;
 var baseUrl = "http://localhost:3000/api/v1/";
 
-describe('return mongoSample', function(){
-	it('return mongoSample', function(done){
-		request.get({url: baseUrl + 'mongoSample'}, function(err, res, body){
-			expect(res.statusCode).to.equal(200);
-			console.log(body);
+describe('test mongoSample get', function(){
+	it('test mongoSample get', function(done){
+		request(baseUrl)
+		.get('mongoSample')
+		.expect(200)
+		.expect(function(res){
+			console.log(res.body);
+		})
+		.end(function(err, res) {
+			done();
+		});
+	});
+});
+
+describe('test mongoSample post', function(){
+	it('test mongoSample get', function(done){
+		request(baseUrl)
+		.post('mongoSample')
+		.send({"name": "tom","age": 6,"gender": "female"})
+		.expect(200)
+		.end(function(err, res) {
+			var json = JSON.parse(res.text);
+			json.name.should.be.equal('tom');
+			json.age.should.be.equal(6);
+			json.gender.should.be.equal('female');
 			done();
 		});
 	});
